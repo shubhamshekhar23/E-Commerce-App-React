620d13ea29a304a18f399baaf2065fb41f0ee390
Shubham Shekhar <shubham.ae.iitkgp@gmail.com>
2023-11-25 19:57:03 +0530

    details updated

--- src/App.js
+++ src/App.js
@@ -27,6 +27,38 @@ Technical Requirement
 4. No need to update the UX.
  */
 
+/* 
+
+
+* APP Details - 
+
+## Working Details:
+
+- Upon app loading, it retrieves the initial set of 10 products and displays them. No default category is selected.
+- When the user scrolls vertically, the app dynamically fetches products in batches of 10 until there are no more items to retrieve.
+- If a user selects a category:
+  - The URL is updated, incorporating the category name as a query parameter.
+  - Products belonging to the selected category are fetched and displayed.
+  - The app supports infinite scrolling for category-based product fetching as well.
+  - Note: The current API limitation shows only 5 items per category, hindering comprehensive testing.
+  - Upon refreshing the page, the category-based functionality operates seamlessly.
+- In the case of user-initiated searches on input-field:
+  - Products matching the search query are fetched.
+  - A 500ms debounce is applied to the search input.
+  - The URL is updated with the search input as query param to reflect the ongoing search.
+  - If a category is selected while searching, the results are filtered based on both the category and search query.
+  - the above filtering is done on the client side as searchQuery api doesn't support category filter.
+  - Upon refreshing the page, the search functionality remains effective.
+
+## Architecture and Folder Structure:
+
+- The `src/hooks` directory contains custom hooks designed for use across multiple components.
+- The `src/components/hooks` directory consists of hooks specific to individual components (not shared among multiple components).
+- `action-services` are responsible for updating the store, and components utilize these services.
+- The `services/api` directory houses services dedicated to making API calls using Axios.
+
+*/
+
 function App() {
   const { products } = useSelector((state) => state.products);
   const { categories } = useSelector((state) => state.categories);
