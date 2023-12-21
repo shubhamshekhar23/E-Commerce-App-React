e2c963d82348666c4eb575628d4f9cef08f17d4f
Shubham Shekhar <shubham.ae.iitkgp@gmail.com>
2023-12-22 04:31:10 +0530

    button used for category item

--- src/components/Category.js
+++ src/components/Category.js
@@ -2,42 +2,27 @@ import React, { useEffect, useState, useRef } from "react";
 import { useSelector } from "react-redux";
 import { useUrlHook } from "./hooks/useUrlHook";
 
-function CategoryCard(props) {
-  const { name, selectedCategory, handleCategoryChange } = props;
-
-  return (
-    <div key={name} className="category-card">
-      <input
-        type="radio"
-        value={name}
-        id={name}
-        checked={selectedCategory === name}
-        onChange={handleCategoryChange}
-      />
-      <label htmlFor={name}>{name}</label>
-    </div>
-  );
-}
-
 export default function Category(props) {
   const { data } = props;
   const { category } = useSelector((state) => state.searchFilters);
 
   const { updateQueryParamInUrl } = useUrlHook();
 
-  const handleCategoryChange = (event) => {
-    updateQueryParamInUrl("category", event.target.value);
+  const handleCategoryChange = (value) => {
+    if (value === category) updateQueryParamInUrl("category", "");
+    else updateQueryParamInUrl("category", value);
   };
 
   return (
     <>
       {data.map((name) => (
-        <CategoryCard
+        <button
+          className={`filter-button ${name == category ? `active` : ``}`}
           key={name}
-          name={name}
-          selectedCategory={category}
-          handleCategoryChange={handleCategoryChange}
-        />
+          onClick={() => handleCategoryChange(name)}
+        >
+          {name}
+        </button>
       ))}
     </>
   );
