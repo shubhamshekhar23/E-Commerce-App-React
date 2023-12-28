ad52da17f993162b6e0dc95fe96560a9c843c581
Shubham Shekhar <shubham.ae.iitkgp@gmail.com>
2023-12-28 18:10:29 +0530

    whatver

--- src/index.js
+++ src/index.js
@@ -11,7 +11,12 @@ import "./styles.css";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+const router = createBrowserRouter(
+  [{ path: "/E-Commerce-App-React", element: <App /> }],
+  {
+    // basename: "/E-Commerce-App-React", // Set your desired basename here
+  }
+);
 
 root.render(
   <Provider store={store}>
